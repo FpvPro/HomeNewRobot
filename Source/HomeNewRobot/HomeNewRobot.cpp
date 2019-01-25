845e7de6457dc071ecee5e352c8871a848e25365
FpvPro <37640706+FpvPro@users.noreply.github.com>
2019-01-25 18:30:22 -0500

    Initial commit

---
+++ Source/HomeNewRobot/HomeNewRobot.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "HomeNewRobot.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HomeNewRobot, "HomeNewRobot" );
